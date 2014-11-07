b48975273a18bc8d32263e182a9fbada582f9985
nealhamilton <neal.robert.hamilton@gmail.com>
2014-11-07 10:16:15 -0500

    Updated logIn.py
    added comments to code

--- logIn.py
+++ logIn.py
@@ -1,16 +1,16 @@
+# logIn.py
+# Neal Hamilton
+# 11.7.14
+# simple login started for mail center app
 
 
-
+# imports
 from Tkinter import *
 
 
 def main():
     login = Tk()
     login.title('Login')
-
-    # button events
-    def button_click():
-        print 'clicked'
 
     # labels
     id_label = Label(login, text='Id number: ').grid(row=0)
@@ -20,9 +20,14 @@ def main():
     id_num = Entry(login, bd=5).grid(row=0, column=1)
     pword = Entry(login, bd=5).grid(row=1, column=1)
 
+    # button events
+    def button_click():
+        print 'clicked'
+
     # button
     login_button = Button(login, text='Login', command=button_click).grid(row=2, column=1)
 
+    # waiting for user's input
     login.mainloop()
 
 if __name__ == "__main__":
